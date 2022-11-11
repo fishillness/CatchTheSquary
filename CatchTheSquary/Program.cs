e5f4d133dac7cbb3d0424e18af8600e63e98f262
fishillness <fishillness@gmail.com>
2022-11-11 18:09:28 +0700

    Billet

--- CatchTheSquary/Program.cs
+++ CatchTheSquary/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CatchTheSquary
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        RenderWindow window = new RenderWindow(new VideoMode(800, 600), "Game");
+        window.Closed += Window_Closed;
+        window.SetFramerateLimit(60);
+
+        while (window.IsOpen == true)
         {
+            window.Clear(new Color(230, 230, 230));
+
+            window.DispatchEvents();
+
+            window.Display();
         }
+    }
+
+    private static void Window_Closed(object sender, EventArgs e)
+    {
+        ((RenderWindow)sender).Close();
     }
 }
